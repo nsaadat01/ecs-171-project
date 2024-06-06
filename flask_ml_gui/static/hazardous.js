62c95a6bc9d3577574df619e6d5824096b2f63ee
Noor-Aysha Saadat <naysaadat@gmail.com>
2024-06-05 21:21:20 -0700

    got the final versions of the results

--- flask_ml_gui/static/hazardous.js
+++ flask_ml_gui/static/hazardous.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     if (playPromise !== undefined) {
         playPromise.then(_ => {
             console.log("Audio started playing automatically.");
-        }).catch(error => {
+        }).catch(_ => {
             console.log("Auto-play was prevented. Consider triggering playback with a user gesture.");
         });
     }
@@ -35,28 +35,39 @@ document.addEventListener('DOMContentLoaded', function() {
                     }, delay);
                     delay += 500;
                 }
+
+                setTimeout(() => {
+
+                    // Pause with a black screen
+                    finalContainer.style.display = 'none';
+
+                    setTimeout(() => {
+                        const thirdContainer = document.getElementById('third-container');
+                        thirdContainer.style.display = 'block';
+        
+                        let blinkInterval = setInterval(() => {
+                            thirdContainer.style.opacity = (thirdContainer.style.opacity == 0 ? 1 : 0);
+                        }, 1000);
+        
+                        // Listen for any key press or click to navigate to index.html
+                        document.addEventListener('keydown', navigateHome);
+                        document.addEventListener('click', navigateHome);
+        
+                        function navigateHome() {
+                            clearInterval(blinkInterval);
+                            window.location.href = '/';
+                            document.removeEventListener('keydown', navigateHome);
+                            document.removeEventListener('click', navigateHome);
+                        }
+                        
+                    }, 3000);  // Wait longer before hiding the third container
+
+                }, delay + 5000); // cause the screen to go black before showing the <press button>
+
             }, 500 * firstline.length);  // Ensure we start after the last of the first line
 
-            setTimeout(() => {
-                const thirdContainer = document.getElementById('third-container');
-                finalContainer.style.display = 'none';
-                thirdContainer.style.display = 'block';
-
-                let blinkInterval = setInterval(() => {
-                    thirdContainer.style.opacity = (thirdContainer.style.opacity == 0 ? 1 : 0);
-                }, 1000);
-
-                // Listen for any key press or click to navigate to index.html
-                document.addEventListener('keydown', navigateHome);
-                document.addEventListener('click', navigateHome);
-
-                function navigateHome() {
-                    clearInterval(blinkInterval);
-                    window.location.href = 'index.html';
-                    document.removeEventListener('keydown', navigateHome);
-                    document.removeEventListener('click', navigateHome);
-                }
-            }, delay + 8000);  // Wait longer before hiding the final container
         }, 2000); // delay between displaying stuff
-    }, 30500);  // Alert container displayed for 30 seconds
+
+    }, 30500);  // Alert container displayed for 30 seconds // FOR TESTING: TODO
+
 });
